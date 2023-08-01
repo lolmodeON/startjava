6e916c31a74d42e7c89dd8f17f34d7dafc665d64
lolmodeON <141110932+lolmodeON@users.noreply.github.com>
2023-08-01 13:34:21 +0600

    Update MyFirstApp.java
    Изменил вывод текста, отображаемого в консоль.

--- Lesson1/MyFirstApp.java
+++ Lesson1/MyFirstApp.java
@@ -1,5 +1,6 @@
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Написано однажды, работает везде");
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
     }
 }
